4f1c76f6eb9a07e8da35969c6da276e161721e1e
unknown <pavanalla02@gmail.com>
2023-08-29 14:34:42 +0530

    first commit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+/**
+ * welcome to diet college!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "welcome to diet college college!" );
+    }
+}
